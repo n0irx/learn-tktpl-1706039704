6b300308db0d284987ccb53cfe96daf9280283a3
nataprawiraf <nataprawiraf@gmail.com>
2020-10-02 10:28:28 +0700

    override back button to confirm on exit

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/lab/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/lab/MainActivity.kt
@@ -1,11 +1,13 @@
 package id.ac.ui.cs.mobileprogramming.nataprawiraf.lab
 
 import android.os.Bundle
+import android.os.Handler
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,5 +36,18 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private var doubleBackToExitPressedOnce = false
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
     }
 }
