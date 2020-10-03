4303f5fb3db461c01e7a5201796e456055c0c9e0
nataprawiraf <nataprawiraf@gmail.com>
2020-10-03 09:50:10 +0700

    add basic placeholder function and TODO

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/lab/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/lab/MainActivity.kt
@@ -1,20 +1,70 @@
 package id.ac.ui.cs.mobileprogramming.nataprawiraf.lab
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
+import android.os.health.TimerStat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    enum class TimerState {
+        Stopped, Paused, Running
+    }
+
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds = 0L
+    private var timerState = TimerState.Stopped
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setIcon(R.drawable.ic_timer)
+        supportActionBar?.title = "Timer"
+
+        fab_start.setOnClickListener {
+            startTimer()
+            timerState = TimerState.Running
+        }
+
+        fab_pause.setOnClickListener {
+            timer.cancel()
+            timerState = TimerState.Paused
+            updateButtons()
+        }
+
+        fab_stop.setOnClickListener {
+            timer.cancel()
+            onTimerFinished()
+        }
+
+    }
+
+    override fun onResume () {
+        super.onResume()
+        initTimer()
+        TODO("remove background timer and hide notif")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if(timerState == TimerState.Running) {
+            timer.cancel()
+            TODO("start background timer and show notif ")
+        }
+        else if(timerState == TimerState.Paused) {
+            TODO("start background timer and show notif ")
+        }
+
+
 
     }
 
