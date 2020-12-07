28da246893d9d88b02aadce178e60fa07825e5f6
nataprawiraf <nataprawiraf@gmail.com>
2020-12-07 23:52:57 +0700

    remove unecesarry codes

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,8 @@
 #include <jni.h>
 
-//
-// Created by Needfeed on 07/12/2020.
-//
-
-
 extern "C" jint
-Java_id_ac_ui_cs_mobileprogramming_nataprawiraf_lab_FirstFragment_add(JNIEnv *env, jobject thiz, jint a,
-                                                                jint b) {
+Java_id_ac_ui_cs_mobileprogramming_nataprawiraf_lab_FirstFragment_add(JNIEnv *env,
+        jobject thiz, jint a,
+        jint b) {
     return a + b;
 }
